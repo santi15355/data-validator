d63d26923504702c235e3aec3d708afdec4bc1d9
vasiliyChobot <i@vchobot.ru>
2022-10-12 18:51:58 +0300

    added required() and contains() methods, added tests

--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -2,31 +2,36 @@ package hexlet.code;
 
 public final class StringSchema {
     private static String status = "not required";
+    private static String data;
 
-    public StringSchema() {
-    }
-
-    public static void setStatus() {
-        status = status;
-    }
-
-    public static String getStatus() {
+    public String getStatus() {
         return status;
+    }
+
+    public void setStatus(String flag) {
+        StringSchema.status = flag;
     }
 
     public boolean isValid(Object obj) {
         if (status.equals("not required")) {
             return true;
         }
-        return false;
+        if (status.equals("required") && obj == null || obj == "") {
+            return false;
+        }
+        if (status.equals("contains")) {
+            return ((String) obj).contains(data);
+        }
+        return true;
     }
 
     public void required() {
         status = "required";
     }
 
-    public void contains() {
+    public StringSchema contains(String text) {
         status = "contains";
-
+        StringSchema.data = text;
+        return this;
     }
 }
